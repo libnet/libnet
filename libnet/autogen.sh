57fb8815c8be18fca161ca709bc41e1bde4e0f07
Sam Roberts <vieuxtech@gmail.com>
2009-03-26 18:15:35 -0700

    merged autogen.sh from 1.1.3, now ltmain.sh comes from autogen.sh

--- libnet/autogen.sh
+++ libnet/autogen.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
+#
+# A bootstrapping script that can be used to generate the autoconf,
+# automake and libtool-related scripts of the build process.
+#
+
+trap "/bin/rm -fr autom4te.cache ; \
+    echo 'Failed to regenerate autoconf/automake stuff.'" 0 1 2 15
 
 set -x
+set -e
 
-aclocal
+rm -fr autom4te.cache
+libtoolize --copy --force
+aclocal -I .
 autoconf
 autoheader
-automake --add-missing
+automake --add-missing -a -c --foreign
+
+set +e
+trap "echo $0 ok" 0 1 2 15
 
