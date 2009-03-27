6ca91f5232e0ce954c38eadf197ac3cc2aa6dad7
Sam Roberts <vieuxtech@gmail.com>
2009-03-26 18:19:20 -0700

    minimize diffs between 1.1.3 autogen.sh and here

--- libnet/autogen.sh
+++ libnet/autogen.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/sh -ex
 #
 # A bootstrapping script that can be used to generate the autoconf,
 # automake and libtool-related scripts of the build process.
 #
+# $Id: autogen.sh,v 1.2 2004/08/10 13:57:46 gkeramidas Exp $
 
 trap "/bin/rm -fr autom4te.cache ; \
-    echo 'Failed to regenerate autoconf/automake stuff.'" 0 1 2 15
+      echo 'Failed to regenerate autoconf/automake stuff.'" 0 1 2 15
 
 set -x
 set -e
@@ -13,10 +14,10 @@ set -e
 rm -fr autom4te.cache
 libtoolize --copy --force
 aclocal -I .
-autoconf
 autoheader
-automake --add-missing -a -c --foreign
+automake -a -c --foreign
+autoconf
+rm -fr autom4te.cache
 
 set +e
 trap "echo $0 ok" 0 1 2 15
-
