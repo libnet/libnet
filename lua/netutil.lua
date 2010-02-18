69bee641d27fc335e3de89074a2fe27abcee7dbc
Sam Roberts <vieuxtech@gmail.com>
2010-02-18 14:42:34 -0800

    Factored pcap reencoding out, and implemented reencoding test on pcaps.

--- lua/netutil.lua
+++ lua/netutil.lua
@@ -1,3 +1,5 @@
+require"pcap"
+require"net"
 require"tostring"
 
 -- Quote a string into lua form (including the non-printable characters from
@@ -18,7 +20,11 @@ end
 q = quote
 
 -- binary to hex
+-- binary to hex
 function h(s)
+    if s == nil then
+        return "nil"
+    end
     local function hex(s)
         return string.format("%02x", string.byte(s))
     end
@@ -38,5 +44,68 @@ function b(s)
 
     local s = s:gsub("(..)", cvt)
     return s
+end
+
+function countdiff(s0, s1)
+  assert(#s0 == #s1)
+  local count = 0
+  for i=1,#s0 do
+    if string.byte(s0, i) ~= string.byte(s1, i) then
+      count = count + 1
+    end
+  end
+  return count
+end
+
+function assertmostlyeql(threshold, s0, s1)
+  assert(#s0 == #s1)
+  local diff = countdiff(s0, s1)
+  assert(diff <= threshold, diff)
+end
+
+function pcapreencode(incap, outcap)
+    if not outcap then
+        outcap = "reencoded-"..incap
+    end
+    os.remove(outcap)
+
+    local cap = assert(pcap.open_offline(incap))
+    local dmp = assert(cap:dump_open(outcap))
+    local n = assert(net.init())
+    local i = 0
+    for pkt, time, len in cap.next, cap do
+        i = i + 1
+        print("packet", i, "wirelen", len, "timestamp", time, os.date("!%c", time))
+        assert(n:clear())
+        assert(n:decode_eth(pkt))
+        assert(dmp:dump(n:block(), time, len))
+    end
+    -- FIXME assert(i > 0)
+    dmp:close()
+    cap:close()
+    n:destroy()
+    return outcap
+end
+
+function assertpcapsimilar(threshold, file0, file1)
+    local n0 = assert(net.init())
+    local n1 = assert(net.init())
+    local cap0 = assert(pcap.open_offline(file0))
+    local cap1 = assert(pcap.open_offline(file1))
+    local i = 0
+    for pkt0, time0, len0 in cap0.next, cap0 do
+        local pkt1, time1, len1 = assert(cap1:next())
+        i = i + 1
+
+        print("packet0", i, "wirelen", len0, "timestamp", time0, os.date("!%c", time0))
+        print("packet1", i, "wirelen", len1, "timestamp", time1, os.date("!%c", time1))
+
+        assert(len0 == len1)
+        assert(time0 == time1, string.format("%.7f ~= %.7f", time0, time1))
+        assertmostlyeql(threshold, pkt0, pkt1)
+    end
+    assert(cap1:next() == nil)
+    n0:destroy()
+    n1:destroy()
 end
 
