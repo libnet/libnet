53e3857740cd7f928cea858778ce67003e6498ad
Sam Roberts <vieuxtech@gmail.com>
2010-04-01 13:26:31 -0700

    Further simplify autogen.sh

--- libnet/autogen.sh
+++ libnet/autogen.sh
@@ -5,24 +5,8 @@
 #
 # $Id: autogen.sh,v 1.2 2004/08/10 13:57:46 gkeramidas Exp $
 
-trap "/bin/rm -fr autom4te.cache ; \
-      echo 'Failed to regenerate autoconf/automake stuff.'" 0 1 2 15
-
 set -x
 set -e
 
-rm -fr autom4te.cache
 autoreconf -ivf -I . -I /sw/share/aclocal
 
-# Above should obsolete the below:
-# libtoolize --force
-# aclocal -I . -I /sw/share/aclocal || aclocal -I .
-# autoheader
-# automake --foreign --force --add-missing --copy
-#libtoolize --force
-# autoconf
-
-rm -fr autom4te.cache
-
-set +e
-trap "echo $0 ok" 0 1 2 15
