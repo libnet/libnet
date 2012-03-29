5721f9862674e7c647908d9070f9a280c66d92c8
Sam Roberts <vieuxtech@gmail.com>
2012-03-29 13:43:24 -0700

    Generate a valid IGMP message in test.

--- lua/test-igmp.lua
+++ lua/test-igmp.lua
@@ -2,12 +2,12 @@ dofile"setup.lua"
 
 test("+igmp", function()
   local n = net.init()
-  local igmpt = {type=3, code=5, ip="1.2.3.4"}
+  local igmpt = {type=0x11, code=0, ip="1.2.3.4"}
   local ipt = {src="1.2.3.4", dst="5.6.7.8", protocol=2}
   local etht = {src="01:02:03:01:02:03", dst="04:05:06:04:05:06"}
   local pkt = "0405060405060102030102030800".. -- eth
               "4500001c0000000040026acd0102030405060708".. -- ip
-              "0305f6f604030201" -- igmp
+              "1100eaf901020304" -- igmp
 
   print"  w/no-payload"
 
