libnet/libnet

commit 27282bb5a1ab78210245e226c4c82605b00a12bb

Make libnet_get_hwaddr work in the last few releases of OpenBSD (stu@spacehopper.org)

libnet/src/libnet_link_bpf.c
@@ -43,8 +43,10 @@
 #include <sys/sysctl.h>
 #include <net/route.h>
 #include <net/if_dl.h>
+#include <net/if_types.h>
 #include "../include/gnuc.h"
-#include "../include/bpf.h"
+
+#include <bpf.h>
 
 #ifdef HAVE_OS_PROTO_H
 #include "../include/os-proto.h"
@@ -308,9 +310,13 @@ libnet_get_hwaddr(libnet_t *l)
     for (next = buf ; next < end ; next += ifm->ifm_msglen)
     {
         ifm = (struct if_msghdr *)next;
+        if (ifm->ifm_version != RTM_VERSION)
+            continue;
         if (ifm->ifm_type == RTM_IFINFO)
         {
             sdl = (struct sockaddr_dl *)(ifm + 1);
+            if (sdl->sdl_type != IFT_ETHER)
+                continue;
             if (strncmp(&sdl->sdl_data[0], l->device, sdl->sdl_nlen) == 0)
             {
                 if (!(ea = malloc(sizeof(struct libnet_ether_addr))))

commit 408fa2266a4af402152cc0f1e9a40b56477b995a

Make libnet_get_hwaddr() work with 802.1q interfaces in bpf (BSD).
This fixes a regression in introduced in
27282bb5a1ab78210245e226c4c82605b00a12bb.

libnet/src/libnet_link_bpf.c
@@ -314,7 +314,11 @@ libnet_get_hwaddr(libnet_t *l)
         if (ifm->ifm_type == RTM_IFINFO)
         {
             sdl = (struct sockaddr_dl *)(ifm + 1);
-            if (sdl->sdl_type != IFT_ETHER)
+            if (sdl->sdl_type != IFT_ETHER
+                && sdl->sdl_type != IFT_FASTETHER
+                && sdl->sdl_type != IFT_FASTETHERFX
+                && sdl->sdl_type != IFT_GIGABITETHERNET
+                && sdl->sdl_type != IFT_L2VLAN)
                 continue;
             if (strncmp(&sdl->sdl_data[0], l->device, sdl->sdl_nlen) == 0)
             {
@@ -324,6 +328,12 @@ libnet_get_hwaddr(libnet_t *l)
         }
     }
     free(buf);
+    if (next == end) {
+        snprintf(l->err_buf, LIBNET_ERRBUF_SIZE,
+                 "%s(): interface %s of known type not found.",
+                 __func__, l->device);
+        return NULL;
+    }
     return (&ea);
 }
 