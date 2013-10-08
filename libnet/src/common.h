libnet/libnet

commit a89f0f10de9b70c0bcc0ab31552a12e1a8b83921

Fix #34 correct checksum caculation when IPv6 extension headers being used

libnet/src/common.h
@@ -43,6 +43,9 @@
  * included.
  */
 #include <netinet/in.h>
+#ifndef IPPROTO_MH		
+#define IPPROTO_MH		135	/* IPv6 mobility header		*/
+#endif
 
 /* TODO - should ../include/gnuc.h be included here? */
 

libnet/src/libnet_checksum.c
@@ -201,11 +201,39 @@ libnet_inet_checksum(libnet_t *l, uint8_t *iphdr, int protocol, int h_len, const
         ip6h_p = (struct libnet_ipv6_hdr *)iph_p;
         iph_p = NULL;
         ip_hl   = 40;
+        uint8_t ip_nh = ip6h_p->ip_nh;
+
         if((uint8_t*)(ip6h_p+1) > end)
         {
             snprintf(l->err_buf, LIBNET_ERRBUF_SIZE,
                     "%s(): ipv6 hdr not inside packet", __func__);
             return -1;
+        }
+
+        while (ip_nh != protocol && (uint8_t*)ip6h_p + ip_hl + 1 < end)
+        { /* next header is not the upper layer protocol */
+           switch (ip_nh)
+           {
+              case IPPROTO_DSTOPTS:
+              case IPPROTO_HOPOPTS:
+              case IPPROTO_ROUTING:
+              case IPPROTO_FRAGMENT:
+              case IPPROTO_AH:
+              case IPPROTO_ESP:
+              case IPPROTO_MH:
+                 /*
+                  * count option headers to the header length for 
+                  * checksum processing
+                  */
+                 ip_nh = *((uint8_t*)ip6h_p+ip_hl); /* next next header */
+                 ip_hl += (*((uint8_t*)ip6h_p+ip_hl+1)+1)*8; /* ext header length */
+                 break;
+              default:
+                 snprintf(l->err_buf, LIBNET_ERRBUF_SIZE,
+                     "%s(): unsupported extension header (%d)", __func__, ip_nh);
+                 return -1;
+           }
+
         }
     }
     else

commit 0717d9c0697dddfbea4b91704deaf4af5970f4e6

Fix errors with missing IPPROTO_MH on windows
Use of IPPROTO_MH was introduced with a89f0f10de, but definition
was conditionalized to non-windows platforms.

libnet/src/common.h
@@ -38,14 +38,7 @@
 
 #include <sys/types.h>
 
-/* IPPROTO_ and sockaddr_ definitions are here. They are often
- * implicitly pulled in, but some systems need them explicitly
- * included.
- */
 #include <netinet/in.h>
-#ifndef IPPROTO_MH
-#define IPPROTO_MH		135	/* IPv6 mobility header		*/
-#endif
 
 /* TODO - should ../include/gnuc.h be included here? */
 
@@ -56,3 +49,10 @@
 #include "../include/config.h"
 #include "../include/libnet.h"
 
+/* IPPROTO_ and sockaddr_ definitions are here. They are often
+ * implicitly pulled in, but some systems need them explicitly
+ * included.
+ */
+#ifndef IPPROTO_MH
+#define IPPROTO_MH		135	/* IPv6 mobility header		*/
+#endif