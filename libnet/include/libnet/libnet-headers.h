a3feb58ed64a8ef4962bc8e27c17a26dec442dd8
Ali Abdulkadir <autostart.ini@gmail.com>
2017-04-09 16:03:13 +0300

    ETHERTYPE_IPV6 Hex letter digits' are now all uppercase
    Having added diagnostic pragmas around them for clang made gcc warn about unknown pragmas.
    I'm not going to follow this rabbit hole any further... they're uppercase now until I get rid of them altogether.

--- libnet/include/libnet/libnet-headers.h
+++ libnet/include/libnet/libnet-headers.h
@@ -211,7 +211,7 @@ struct libnet_arp_hdr
 #define ARPHRD_IPSEC    31  /* IPsec tunnel */
     uint16_t ar_pro;         /* format of protocol address */
     uint8_t  ar_hln;         /* length of hardware address */
-    uint8_t  ar_pln;         /* length of protocol addres */
+    uint8_t  ar_pln;         /* length of protocol address */
     uint16_t ar_op;          /* operation type */
 #define ARPOP_REQUEST    1  /* req to resolve address */
 #define ARPOP_REPLY      2  /* resp to previous request */
@@ -313,7 +313,7 @@ struct libnet_bgp4_notification_hdr
 struct libnet_cdp_hdr
 {
     uint8_t cdp_version;     /* version (should always be 0x01) */
-    uint8_t cdp_ttl;         /* time reciever should hold info in this packet */
+    uint8_t cdp_ttl;         /* time receiver should hold info in this packet */
     uint16_t cdp_sum;        /* checksum */
     uint16_t cdp_type;       /* type */
 #define LIBNET_CDP_DEVID    0x1 /* device id */
@@ -506,12 +506,7 @@ struct libnet_ethernet_hdr
 #ifndef ETHERTYPE_IP
 #define ETHERTYPE_IP            0x0800  /* IP protocol */
 #endif
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmacro-redefined"
-#define ETHERTYPE_IPV6          0x86dd  /* IPv6 protocol */
-#pragma clang diagnostic pop
-
+#define ETHERTYPE_IPV6          0x86DD  /* IPv6 protocol */
 #ifndef ETHERTYPE_ARP
 #define ETHERTYPE_ARP           0x0806  /* addr. resolution protocol */
 #endif
