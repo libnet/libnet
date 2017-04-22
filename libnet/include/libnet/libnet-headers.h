e297924696f581ced04c707665e31757b9e51ddd
Ali Abdulkadir <autostart.ini@gmail.com>
2017-04-22 08:39:40 +0300

    Put ETHERTYPE_IPV6 in ifdef block
    Just like all other ethertypes.
    Sitting there without it was what was causing the boatloads of macro redefinition warnings (see commit 64d27b and eb76d2)
    I should have though of that earlier...

--- libnet/include/libnet/libnet-headers.h
+++ libnet/include/libnet/libnet-headers.h
@@ -506,7 +506,9 @@ struct libnet_ethernet_hdr
 #ifndef ETHERTYPE_IP
 #define ETHERTYPE_IP            0x0800  /* IP protocol */
 #endif
-#define ETHERTYPE_IPV6          0x86DD  /* IPv6 protocol */
+#ifndef ETHERTYPE_IPV6
+#define ETHERTYPE_IPV6          0x86dd  /* IPv6 protocol */
+#endif
 #ifndef ETHERTYPE_ARP
 #define ETHERTYPE_ARP           0x0806  /* addr. resolution protocol */
 #endif
