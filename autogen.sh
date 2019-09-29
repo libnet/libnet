521b6b221ea707ff225933e2a58834b0b80eeba5
Joachim Nilsson <troglobit@gmail.com>
2019-09-29 12:19:25 +0200

    autogen.sh: Remove script debugging and rebuild if possible
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -ex
+#!/bin/sh
 #
 # A bootstrapping script that can be used to generate the autoconf,
-# automake and libtool-related scripts of the build process.
+# automake and libtool-related scripts of the build process.  Only
+# useful for developers updating Makefile.am, configure.ac etc.
+# Regular users building from released tarballs can use the shipped
+# configure script, that generates Makefile's from Makefile.in's
 
-set -e
-
-autoreconf --force --install $*
+autoreconf -W portability -vifm $*
