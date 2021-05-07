4059e2ef7f277b962dc4a93bd2df9445361c7cca
Valery Ivanov <ivalery111@gmail.com>
2021-05-07 19:28:25 +0300

    feat(functions): add libnet_build_lldp_chassis function

--- include/libnet/libnet-functions.h
+++ include/libnet/libnet-functions.h
@@ -732,6 +732,21 @@ uint16_t value_s, const uint8_t *value, const uint8_t* payload, uint32_t payload
 libnet_t *l, libnet_ptag_t ptag);
 
 /**
+ * Builds a LLDP Chassis ID TLV. The Chassis ID TLV is the _first_ mandatory TLV in the LLDPDU.
+ * @param subtype Chassis ID Subtype
+ * @param value the Chassis ID string
+ * @param value_s length of value argument
+ * @param l pointer to a libnet context
+ * @param ptag protocol tag to modify an existing header, 0 to build a new one
+ * @return protocol tag value on success
+ * @retval -1 on error
+ */
+LIBNET_API
+libnet_ptag_t libnet_build_lldp_chassis(const uint8_t subtype,
+const uint8_t *const value, const uint8_t value_s,
+libnet_t *l, libnet_ptag_t ptag);
+
+/**
  * Builds an IP version 4 RFC 792 Internet Control Message Protocol (ICMP)
  * echo request/reply header
  * @param type type of ICMP packet (should be ICMP_ECHOREPLY or ICMP_ECHO)
