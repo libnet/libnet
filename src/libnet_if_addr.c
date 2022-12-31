libnet/libnet

commit 36e96bfce52fdbcff944d1f9f519677fc0d3b172

src: minor, fix parametner unused in windows backend
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/libnet_if_addr.c
@@ -461,13 +461,15 @@ static int8_t *iptos(uint32_t in)
 }
 
 int
-libnet_ifaddrlist(struct libnet_ifaddr_list **ipaddrp, char *dev_unused, char *errbuf)
+libnet_ifaddrlist(struct libnet_ifaddr_list **ipaddrp, char *dev, char *errbuf)
 {
     struct libnet_ifaddr_list *ifaddrlist = NULL;
     int8_t err[PCAP_ERRBUF_SIZE];
     pcap_if_t *devlist = NULL;
     pcap_if_t *dev = NULL;
     size_t nipaddr = 0;
+
+    (void)dev; /* unused */
 
     /* Retrieve the interfaces list */
     if (pcap_findalldevs(&devlist, err) == -1)

commit b9c4d0f94af9a71698d779e0ed55eb53a51de8d0

Fix win32 regression introduced in 36e96bf
libnet_if_addr.c(467): error C2082: redefinition of formal parameter 'dev'

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/libnet_if_addr.c
@@ -459,7 +459,7 @@ static int8_t *iptos(uint32_t in)
 }
 
 int
-libnet_ifaddrlist(struct libnet_ifaddr_list **ipaddrp, char *dev, char *errbuf)
+libnet_ifaddrlist(struct libnet_ifaddr_list **ipaddrp, char *unused, char *errbuf)
 {
     struct libnet_ifaddr_list *ifaddrlist = NULL;
     int8_t err[PCAP_ERRBUF_SIZE];
@@ -467,7 +467,7 @@ libnet_ifaddrlist(struct libnet_ifaddr_list **ipaddrp, char *dev, char *errbuf)
     pcap_if_t *dev = NULL;
     size_t nipaddr = 0;
 
-    (void)dev; /* unused */
+    (void)unused;
 
     /* Retrieve the interfaces list */
     if (pcap_findalldevs(&devlist, err) == -1)