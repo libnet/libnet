cdda3af90d4606c21e8bfc96ef619218768d1e8c
Joachim Wiberg <troglobit@gmail.com>
2023-10-01 18:40:51 +0200

    test: follow-up to 7a14026, fix macOS 'make check'
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/setup.sh
+++ test/setup.sh
@@ -4,11 +4,13 @@
 # http://staskobzar.blogspot.com/2017/04/test-anything-protocol-tap-with.html
 export CMOCKA_MESSAGE_OUTPUT=TAP
 
-ip link add eth0 type dummy
-ip link set eth0 up
-ip link set lo up
+if [ $(uname) = Linux ]; then
+    ip link add eth0 type dummy
+    ip link set eth0 up
+    ip link set lo up
 
-ip addr  add 192.168.2.200/24 dev eth0
-ip route add default via 192.168.2.1
+    ip addr  add 192.168.2.200/24 dev eth0
+    ip route add default via 192.168.2.1
+fi
 
 exec "$@"
